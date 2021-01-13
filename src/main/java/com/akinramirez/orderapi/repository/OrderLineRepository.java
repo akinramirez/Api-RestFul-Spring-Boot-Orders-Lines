5abb90bfa21cd9ad1ef9f3940878444f4989acf2
akinramirez <akin.ramirez@hotmail.com>
2021-01-13 08:53:54 -0600

    Creando la Entidad Order y OrderLine

---
+++ src/main/java/com/akinramirez/orderapi/repository/OrderLineRepository.java
@@ -0,0 +1,11 @@
+package com.akinramirez.orderapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.akinramirez.orderapi.entity.OrderLine;
+
+@Repository
+public interface OrderLineRepository extends JpaRepository<OrderLine, Long>{
+
+}
---
+++ src/main/java/com/akinramirez/orderapi/repository/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.akinramirez.orderapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.akinramirez.orderapi.entity.Order;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long>{
+	
+}
